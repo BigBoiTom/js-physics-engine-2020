e4061092d8e99ab91fdef3ab655d09043e297687
BigBoiTom <BigBoiTom@users.noreply.github.com>
2020-02-28 14:55:59 +0100

    added 3 circles
    yoohoo

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -8,3 +8,12 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
+
+let myPoint = new Point(500,200,20, "red");
+myPoint.draw(context);
+
+let myPoint2 = new Point(300,200,10, "blue");
+myPoint2.draw(context);
+
+let myPoint3 = new Point(100,200,30, "green");
+myPoint3.draw(context);
